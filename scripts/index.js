658ded8ea051ccc3db86a6ffe40a3f69b5843a3e
AshleyPutman <ashleyput1228@gmail.com>
2024-12-26 23:01:29 -0600

    add js cards

--- scripts/index.js
+++ scripts/index.js
@@ -1,8 +1,8 @@
 const initialCards[
-    {name:'', link:''},
-    {name:'', link:''},
-    {name:'', link:''},
-    {name:'', link:''},
-    {name:'', link:''},
-    {name:'', link:''},
+    {name:'Beach Trip', link:'https://unsplash.com/photos/seashore-under-clear-blue-sky-during-daytime-g0Qdolm3K14'},
+    {name:'Moving Day', link:'https://unsplash.com/photos/a-pile-of-cardboard-boxes-sitting-on-top-of-a-hard-wood-floor-iIzkK-rOEzE'},
+    {name:'Tokyo', link:'https://unsplash.com/photos/people-gathered-outside-buildings-and-vehicles-alY6_OpdwRQ'},
+    {name:'New York', link:'https://unsplash.com/photos/a-view-of-a-city-at-night-from-the-top-of-a-building-MdrJol8olLg'},
+    {name:'Camping in the Mountains', link:'https://unsplash.com/photos/a-dirt-path-in-the-middle-of-a-forest-76OiaaEZJ5k'},
+    {name:'Grand Canyon', link:'https://unsplash.com/photos/a-view-of-the-grand-canyon-of-the-grand-canyon-D391N3cKjiY'},
 ];
