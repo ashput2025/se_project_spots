2372f8f678ef9fcfd822dd1278c0ac9929a3ea64
AshleyPutman <ashleyput1228@gmail.com>
2025-02-17 22:29:25 -0600

    fix showinputerror func

--- scripts/validation.js
+++ scripts/validation.js
@@ -1,44 +1,40 @@
-const showInputError = (formEl, inputEl, errorMsg) =>{
-    const errorMsgID = inputEl.id + "-error";
-    const errorMsgEl = formEl.querySelector(`.${inputEl.id}-error`);
-    errorMsgEl.textContent = errorMsg;
-}
+const showInputError = (formEl, inputEl, errorMsg) => {
+  const errorMsgID = inputEl.id + "-error";
+  const errorMsgEl = document.querySelector("#" + errorMsgID);
+  errorMsgEl.textContent = errorMsg;
+  console.log(errorMsgID);
+};
 
-const checkInputValidity = (formEl, inputEl)=> {
-if (!inputEl.validity.valid) {
+const checkInputValidity = (formEl, inputEl) => {
+  if (!inputEl.validity.valid) {
     showInputError(formEl, inputEl, inputEl.validationMessage);
-} else {
+  } else {
     hideInputError(formEl, inputEl);
-}
-
-}
+  }
+};
 
 const setEventListener = (formEl) => {
-    const inputList = 
-    Array.from(formEl.querySelectorAll('.modal__input'));
+  const inputList = Array.from(formEl.querySelectorAll(".modal__input"));
 
-    const buttonElement = formEl.querySelector('.modal__submit-btn');
+  const buttonElement = formEl.querySelector(".modal__submit-btn");
 
-    console.log(buttonElement);
-    console.log(inputList);
+  // toggleButtonState(inputList, buttonElement);
 
-    // toggleButtonState(inputList, buttonElement);
-    
-    inputList.forEach((inputElement) => {
-      inputElement.addEventListener("input", () => {
-        checkInputValidity(formEl, inputElement);
-       // toggleButtonState(inputList, buttonElement);
-      });
+  inputList.forEach((inputEl) => {
+    inputEl.addEventListener("input", () => {
+      checkInputValidity(formEl, inputEl);
+      // toggleButtonState(inputList, buttonElement);
     });
-}
+  });
+};
 
 /* define togglebuttonState func */
 
 const enableValidation = () => {
-const formList = document.querySelectorAll('.modal__form');
-formList.forEach((formEl) => {
+  const formList = document.querySelectorAll(".modal__form");
+  formList.forEach((formEl) => {
     setEventListener(formEl);
-})
-}
+  });
+};
 
 enableValidation();
